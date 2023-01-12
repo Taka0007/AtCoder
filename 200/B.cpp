6d3c8faed9294497055f184c3494d1b30fb3f709
Taka <103984069+Taka0007@users.noreply.github.com>
2023-01-13 03:58:54 +0900

    Create B.cpp

---
+++ 200/B.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+int main(){
+               
+  int64_t N,k;  
+  cin>>N>>k;
+  int s=0; 
+               
+  while(s<k)             
+  {if(N%200==0){N=N/200;}
+   else{N=(N*10+2)*100;}               
+               
+   s++;
+   }
+   cout<<N<<endl;
+               }
