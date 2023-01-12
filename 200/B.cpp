0f51162ca2653e840233390afbbc3af211aa6cb3
Taka <103984069+Taka0007@users.noreply.github.com>
2023-01-13 04:01:47 +0900

    Update B.cpp

--- 200/B.cpp
+++ 200/B.cpp
@@ -3,17 +3,19 @@
 #include<math.h>
 
 using namespace std;
+
 int main(){
+               int64_t N,k;  cin>>N>>k;
+
+               int s=0; 
+
+               while(s<k)
+               {if(N%200==0){N=N/200;}
+
+               else{N=(N*10+2)*100;}
                
-  int64_t N,k;  
-  cin>>N>>k;
-  int s=0; 
-               
-  while(s<k)             
-  {if(N%200==0){N=N/200;}
-   else{N=(N*10+2)*100;}               
-               
-   s++;
-   }
-   cout<<N<<endl;
+               s++;
+               }
+
+               cout<<N<<endl;
                }
