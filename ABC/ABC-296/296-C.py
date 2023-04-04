88e654d8da3b03309d880185a5f64e5528fd0849
Taka <103984069+Taka0007@users.noreply.github.com>
2023-04-04 18:59:34 +0900

    Create 296-C.py

---
+++ ABC/ABC-296/296-C.py
@@ -0,0 +1,13 @@
+N,X = map(int,input().split())
+num = list(map(int,input().split()))
+ans = 'No'
+
+#数字のsetを作る
+number = set(num)
+
+
+for i in range(N):
+    if num[i]+X in number:
+        ans = 'Yes'
+
+print(ans)
