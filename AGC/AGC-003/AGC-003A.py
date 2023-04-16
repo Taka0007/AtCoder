117a6a1c873d9f96da944ff6495384bdbd5b138b
Taka <103984069+Taka0007@users.noreply.github.com>
2023-04-16 16:00:41 +0900

    Create AGC-003A.py

---
+++ AGC/AGC-003/AGC-003A.py
@@ -0,0 +1,21 @@
+S = input()
+
+if len(set(S))%2!=0:
+    ans = 'No'
+
+elif 'W' not in set(S) and 'N' not in set(S):
+    ans = 'No'
+    
+elif 'E' not in set(S) and 'S' not in set(S):
+    ans = 'No'
+    
+elif 'E' not in set(S) and 'N' not in set(S):
+    ans = 'No'
+    
+elif 'W' not in set(S) and 'S' not in set(S):
+    ans = 'No'
+    
+else:
+    ans = 'Yes'
+
+print(ans)
