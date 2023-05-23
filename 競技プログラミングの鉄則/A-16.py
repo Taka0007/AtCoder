c3dfc4c95a1d520ca34a5d2fe74b08ddf886d48f
Taka <103984069+Taka0007@users.noreply.github.com>
2023-05-23 11:20:19 +0900

    Create A-16.py

---
+++ 競技プログラミングの鉄則/A-16.py
@@ -0,0 +1,11 @@
+N = int(input())
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
+ans = [2**200]*(N+1)
+ans[0] = 0
+ans[1] = A[0]
+
+for i in range(2,N):
+    ans[i] = min(ans[i-1]+A[i-1], ans[i-2]+B[i-2])
+    
+print(ans[N-1])
