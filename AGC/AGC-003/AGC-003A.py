3812384aadbbe721798c83955c8a7dc2b0244b94
Taka <103984069+Taka0007@users.noreply.github.com>
2023-05-23 11:19:31 +0900

    Update AGC-003A.py

--- AGC/AGC-003/AGC-003A.py
+++ AGC/AGC-003/AGC-003A.py
@@ -2,19 +2,14 @@ S = input()
 
 if len(set(S))%2!=0:
     ans = 'No'
-
 elif 'W' not in set(S) and 'N' not in set(S):
     ans = 'No'
-    
 elif 'E' not in set(S) and 'S' not in set(S):
     ans = 'No'
-    
 elif 'E' not in set(S) and 'N' not in set(S):
     ans = 'No'
-    
 elif 'W' not in set(S) and 'S' not in set(S):
     ans = 'No'
-    
 else:
     ans = 'Yes'
 
