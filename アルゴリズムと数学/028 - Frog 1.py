cb0f3fb561fe4565c5ebb495de1ac7a47075621c
Taka0007 <7takacode@gmail.com>
2023-05-25 16:42:18 +0900

    solve 2 problems

---
+++ アルゴリズムと数学/028 - Frog 1.py
@@ -0,0 +1,10 @@
+N = int(input())
+h= list(map(int,input().split()))
+ans = [0]*(N+1)
+ans[0] = 0
+ans[1] = abs(h[0]-h[1])
+
+for i in range(2,N):
+    ans[i] = min(ans[i-1]+abs(h[i]-h[i-1]), ans[i-2]+abs(h[i]-h[i-2]))
+
+print(ans[N-1])
