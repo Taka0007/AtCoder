0f93e06bbf1997d1720a25a31bb92f13b9478e24
Taka0007 <7takacode@gmail.com>
2023-07-08 23:45:53 +0900

    sol ABC309A

---
+++ ABC/ABC-309/ABC309-A.py
@@ -0,0 +1,21 @@
+A,B = map(int,input().split())
+
+if A==1 and B==2:
+    ans = 'Yes'
+    
+elif A==2 and B==3:
+    ans = 'Yes'
+    
+elif A==4 and B==5:
+    ans = 'Yes'
+    
+elif A==7 and B==8:
+    ans = 'Yes'
+
+elif A == 8 and B==9:
+    ans = 'Yes'
+    
+else:
+    ans = 'No'
+    
+print(ans)
