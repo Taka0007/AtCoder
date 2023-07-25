adc64987f576ac22a027563a0716e20c18d8b0ed
Taka0007 <7takacode@gmail.com>
2023-07-25 16:13:36 +0900

    add temple

---
+++ ABC/ABC-270/ABC270-A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+#include<math.h>
+using namespace std;
+
+int main() {
+
+    int A,B;
+    int ans;
+    cin >> A >> B;
+
+    
+
+   
+    return 0;
+}
