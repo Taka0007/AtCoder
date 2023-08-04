1f74986b097edc90d0111f1115c268d75ba68261
Taka0007 <7takacode@gmail.com>
2023-08-04 16:09:36 +0900

    add graph temple

---
+++ 0-Template&Library/Python/graph(adjacency-list).py
@@ -0,0 +1,13 @@
+N,M = map(int,input().split())
+graph = {i: set() for i in range(1, N + 1)}
+num = []
+for i in range(M):
+    a, b = map(int, input().split())
+    num.append((a, b))
+
+# グラフを構築
+for a, b in num:
+    graph[a].add(b)
+    graph[b].add(a)
+print(graph)
+print(graph[1])
