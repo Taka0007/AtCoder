da8641e16534f7ebfd50b5e382f5a46e43c3aaa6
Taka0007 <7takacode@gmail.com>
2023-11-30 04:29:55 +0900

    solve ABC115-A by cpp

---
+++ ABC/ABC-115/ABC115-A.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+#include<math.h>
+using namespace std;
+
+int main() {
+
+    int D;
+    cin >> D;
+
+    if (D == 25) {
+        cout << "Christmas" << endl;
+    } 
+    
+    else if (D == 24) {
+        cout << "Christmas Eve" << endl;
+    } 
+    
+    else if (D == 23) {
+        cout << "Christmas Eve Eve" << endl;
+    } 
+    
+    else if (D == 22) {
+        cout << "Christmas Eve Eve Eve" << endl;
+    }
+    
+    return 0;
+}
