25dbfcec8c25f1c3e1f6d44eb462d21ff8ded0e2
Taka0007 <7takacode@gmail.com>
2023-12-24 23:55:37 +0900

    solve ABC334-A by Python

---
+++ ABC/ABC-334/ABC334-A.py
@@ -0,0 +1,9 @@
+# Problem link : https://atcoder.jp/contests/abc334/tasks/abc334_a
+# Submit link : https://atcoder.jp/contests/abc334/submissions/48830040
+
+B,G = map(int,input().split())
+
+if B > G:
+    print("Bat")
+else:
+    print("Glove")
