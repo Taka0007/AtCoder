f3e211bec19cef4d35d35d84475e4c33da7c3710
Taka <103984069+Taka0007@users.noreply.github.com>
2024-01-11 23:20:16 +0900

    Pythonテンプレート アップデート
    標準入力の部分を追加

--- 0-Template&Library/Python/temple.py
+++ 0-Template&Library/Python/temple.py
@@ -5,11 +5,22 @@ import scipy
 
 
 
-# input
+# input （標準入力）
 #-----------------------
+
+# 文字列を1つ入力
 S = input()
+
+# 整数を1つ入力
 a = int(input())
+
+# 小数を1つ入力
+a = float(input())
+
+# 整数を2つ入力
 a,b = map(int,input().split())
+
+# 整数をリスト形式で入力
 num_list = list(map(int,input().split()))
 # -----------------------
 
