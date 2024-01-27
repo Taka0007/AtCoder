9491373d77a43e0fe8ace2d1a757861d58fe4c0d
Taka <7takacode@gmail.com>
2024-01-27 18:08:14 +0900

    solve ABC331 by Python

--- ABC/ABC-331/ABC331-A.py
+++ ABC/ABC-331/ABC331-A.py
@@ -2,3 +2,19 @@
 M,D = map(int,input().split())
 y,m,d = map(int,input().split())
 
+## ans variables
+ansY = y
+ansM = m
+ansD = d
+
+if d == D:
+    ansD = 1
+    ansM += 1
+    if ansM > M:
+        ansM = 1
+        ansY += 1
+else:
+    ansD += 1
+
+# output
+print(ansY,ansM,ansD,sep=" ")
