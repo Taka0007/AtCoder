d715d492e778721069e31d71ab2ecc65167db0a9
Taka0007 <7takacode@gmail.com>
2024-02-03 03:53:15 +0900

    solve JOIG-2024-A by Python

---
+++ JOIG/2024/本選/A.py
@@ -0,0 +1,15 @@
+# Problem link : https://atcoder.jp/contests/joig2024-open/tasks/joig2024_a
+# Submission link: https://atcoder.jp/contests/joig2024-open/submissions/49895112
+
+# input
+N = int(input())
+S = input()
+
+# judge
+if 'ooo' in S:
+    ans = 'Yes'
+else:
+    ans = 'No'
+    
+# output
+print(ans)
