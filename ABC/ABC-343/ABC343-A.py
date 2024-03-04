f7ae16f960ef6e91f12b76c28f7ac3d27eef0f5b
Taka0007 <7takacode@gmail.com>
2024-03-04 23:52:28 +0900

    solve ABC343-A by Python

---
+++ ABC/ABC-343/ABC343-A.py
@@ -0,0 +1,10 @@
+# Problem link    : https://atcoder.jp/contests/abc343/tasks/abc343_a
+# Submission link : https://atcoder.jp/contests/abc343/submissions/50908374
+
+
+# input
+A,B = map(int,input().split())
+
+# output
+ans = 9 - A - B
+print(ans)
