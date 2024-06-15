9322396b7f2c5d5f4b7fc02ef17accd173e08d7d
Taka <103984069+Taka0007@users.noreply.github.com>
2024-06-16 01:38:09 +0900

    Create ABC358-A.py

---
+++ ABC/ABC-358/ABC358-A.py
@@ -0,0 +1,10 @@
+# Problem link    : https://atcoder.jp/contests/abc358/tasks/abc358_a
+# Submission link : hhttps://atcoder.jp/contests/abc358/submissions/54620044
+
+# input
+S,T = map(str,input().split())
+# output
+if S == 'AtCoder' and T == 'Land':
+    print('Yes')
+else:
+    print('No')
