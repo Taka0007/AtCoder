5760e3d426082bb41a0cb6b9b3beaf3f19a47db4
Taka <103984069+Taka0007@users.noreply.github.com>
2024-08-03 02:50:14 +0900

    【solved】ABC364-C by Python

--- ABC/ABC-364/ABC364-C.py
+++ ABC/ABC-364/ABC364-C.py
@@ -1,5 +1,5 @@
 # Problem link    : https://atcoder.jp/contests/abc364/tasks/abc364_c
-# Submission link : 
+# Submission link : https://atcoder.jp/contests/abc364/submissions/56218489
 
 # input
 N,X,Y = map(int,input().split())
@@ -7,20 +7,25 @@ A = list(map(int,input().split()))
 B = list(map(int,input().split()))
 
 ## solve about A
-ans_a = N+1
+ans_a = 0
+sum_a = 0
 A.sort()
 A.reverse()
-cumulative_sum_A = [0]*(N)
-cumulative_sum_A[0] = A[0]
-for i in range(1,N):
-    cumulative_sum_A[i] = cumulative_sum_A[i-1] + A[i]
-
-
+for i in range(N):
+    sum_a += A[i]
+    ans_a += 1
+    if sum_a > X:
+        break
 ## solve about B
-
-
-
-
+ans_b = 0
+sum_b = 0
+B.sort()
+B.reverse()
+for i in range(N):
+    sum_b += B[i]
+    ans_b += 1
+    if sum_b > Y:
+        break
 # output
 ans = min(ans_a, ans_b)
 print(ans)
