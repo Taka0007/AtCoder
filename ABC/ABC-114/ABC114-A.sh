52732295dfee6fde07665d0f27172799547e52c4
Taka <103984069+Taka0007@users.noreply.github.com>
2024-10-03 02:39:19 +0900

    Create ABC114-A.sh

---
+++ ABC/ABC-114/ABC114-A.sh
@@ -0,0 +1,16 @@
+# Problem link    : https://atcoder.jp/contests/abc114/tasks/abc114_a
+# Submission link : https://atcoder.jp/contests/abc114/submissions/58355705
+
+read -r X
+
+if [ $X == 7 ]; then
+  ans="YES"
+elif [ $X == 5 ]; then
+  ans="YES"
+elif [ $X == 3 ]; then
+  ans="YES"
+else
+  ans="NO"
+fi
+
+echo $ans
