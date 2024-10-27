f7e62c3b04fd4be024a910f7566e9930093fafdc
Taka <103984069+Taka0007@users.noreply.github.com>
2024-10-27 23:48:21 +0900

    Create ABC377_A.py

---
+++ ABC/ABC_377/ABC377_A.py
@@ -0,0 +1,10 @@
+# Problem link    : https://atcoder.jp/contests/abc377/tasks/abc377_a
+# Submission link : https://atcoder.jp/contests/abc377/submissions/59215096
+
+# input
+S = input()
+# output
+if sorted(S) == ["A","B","C"]:
+    print("Yes")
+else:
+    print("No")
