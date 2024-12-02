7bb8d9b474b230387345038c3b6a4f46bc21123c
Taka <103984069+Taka0007@users.noreply.github.com>
2024-12-03 03:49:38 +0900

    Create ABC382_A.py

---
+++ ABC/ABC_382/ABC382_A.py
@@ -0,0 +1,9 @@
+# Problem link    : https://atcoder.jp/contests/abc382/tasks/abc382_a
+# Submission link : https://atcoder.jp/contests/abc382/submissions/60401036
+
+# input
+N,D = map(int,input().split())
+S = input()
+# output
+ans = S.count('.') + D
+print(ans)
